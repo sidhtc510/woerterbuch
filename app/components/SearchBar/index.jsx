11d8cf3b49f89c4d1329dcaf9904458be49fe130
sidhtc510 <sidhtc510@gmail.com>
2024-06-18 18:21:16 +0200

    .

--- app/components/SearchBar/index.jsx
+++ app/components/SearchBar/index.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 
 export default function SearchBar() {
-  return (
-    <div className=' fixed w-full bg-indigo-300 z-10 bottom-0 border border-indigo-600 p-5'>
-        <input type="text" className='w-full rounded-lg p-4 text-xxl text-indigo-900 font-bold' placeholder='Nachschlagen das Wort' />
-    </div>
-  )
+    const [targetWord, setTargetWord] = useState('');
+
+    return (
+        <div className=' fixed w-full bg-indigo-300 z-10 bottom-0 border border-indigo-600 p-5'>
+            <input type="text" className='w-full rounded-lg p-4 text-xxl text-indigo-900 font-bold' value={targetWord} placeholder='Nachschlagen das Wort' onChange={(e) => setTargetWord(e.target.value)} />
+        </div>
+    )
 }
