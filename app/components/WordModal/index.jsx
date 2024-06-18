a925764be631020d5f0e50b96d247f3451e3621d
sidhtc510 <sidhtc510@gmail.com>
2024-06-18 17:46:57 +0200

    .

--- app/components/WordModal/index.jsx
+++ app/components/WordModal/index.jsx
@@ -23,7 +23,7 @@ export default function WordModal({ wordObj }) {
 
             {isModalOpen && (
                 <div className="fixed inset-0 z-20 overflow-auto bg-black bg-opacity-40 flex overlayClass" onClick={handleCloseModal}>
-                    <div className="bg-indigo-600 m-auto border border-indigo-800 w-4/5 rounded-lg shadow"> {/* my-16 p-5 */}
+                    <div className="bg-indigo-600 m-auto border border-indigo-800 w-5/5 rounded-lg shadow"> {/* my-16 p-5 */}
                         <h2 className='text-xl p-5'>{`${wordObj.word} (${wordObj.wordRu})`}</h2>
                         {/* <table className='table-auto border border-slate-400 border-separate border-spacing-2'> */}
                         <table className='table-auto w-full border-collapse'>
