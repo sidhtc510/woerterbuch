9b355f7043e2098c3382bd817d8246e9bffddf59
sidhtc510 <sidhtc510@gmail.com>
2024-06-19 19:39:01 +0200

    .

--- app/(pages)/addword/page.jsx
+++ app/(pages)/addword/page.jsx
@@ -1,26 +1,40 @@
+'use client'
 import Link from 'next/link'
 import React from 'react'
 
 export default function AddWord() {
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const word = e.target.word.value.trim(); // режем пробелы
+        const correctWord = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase(); //делаем первую букву большой остальные мал  
+        const firstLetter = word[0]; // определяем первую букву слова после трима
+
+        let data = new FormData(e.target)
+        let obj = Object.fromEntries(data)
+
+        console.log(obj);
+    }
+
     return (
         <div>
             <Link href='/' className="w-fit pointer-events-auto rounded-md bg-indigo-600 px-3 py-2 text-[0.8125rem] font-semibold leading-5 text-white hover:bg-indigo-500 cursor-pointer select-none m-3 block">Main page</Link>
 
             <div className='m-3'>
-                <form class="w-full max-w-lg">
+                <form class="w-full max-w-lg" onSubmit={(e) => handleSubmit(e)}>
                     <div class="flex flex-wrap -mx-4 mb-8">
                         <div class=" w-1/2 px-3 mb-3">
                             <label class="block uppercase tracking-wide text-gray-700 text-xs  mb-2" for="word">
                                 Word (without artikle!)
                             </label>
-                            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="word" type="text" placeholder="Word " />
+                            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="word" type="text" placeholder="Word " name="word" />
                             {/* <p class="text-red-500 text-xs italic">Please fill out this field.</p> */}
                         </div>
                         <div class=" w-1/2 px-3">
                             <label class="block uppercase tracking-wide text-gray-700 text-xs  mb-2" for="ruword">
                                 Ru Word translation
                             </label>
-                            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" id="ruword" type="text" placeholder="Translation" />
+                            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" id="ruword" type="text" placeholder="Translation" name="ruword" />
                         </div>
                     </div>
 
@@ -31,11 +45,11 @@ export default function AddWord() {
                                 Nominativ
                             </label>
                             <div class="relative">
-                                <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                                <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state" name="nominative_artikle">
                                     <option selected disabled>Artikel</option>
-                                    <option>der</option>
-                                    <option>das</option>
-                                    <option>die</option>
+                                    <option value="der">der</option>
+                                    <option value="das">das</option>
+                                    <option value="die">die</option>
                                 </select>
                                 <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                                     <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
@@ -52,7 +66,7 @@ export default function AddWord() {
                             <label class="block uppercase tracking-wide text-gray-700 text-xs  mb-2" for="nom_plur">
                                 Plural
                             </label>
-                            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" id="nom_plur" type="text" placeholder="Plural" />
+                            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" id="nom_plur" name="nom_plur" type="text" placeholder="Plural" />
                         </div>
                     </div>
 
@@ -64,10 +78,10 @@ export default function AddWord() {
                                 Genitiv
                             </label>
                             <div class="relative">
-                                <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                                <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state" name="genetiv_artikle">
                                     <option selected disabled>Artikel</option>
-                                    <option>der</option>
-                                    <option>des</option>
+                                    <option value="der">der</option>
+                                    <option value="des">des</option>
                                 </select>
                                 <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                                     <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
@@ -78,13 +92,13 @@ export default function AddWord() {
                             <label class="block uppercase tracking-wide text-gray-700 text-xs  mb-2" for="gen_sing">
                                 Singular
                             </label>
-                            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" id="gen_sing" type="text" placeholder="Singular" />
+                            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" id="gen_sing" name="gen_sing" type="text" placeholder="Singular" />
                         </div>
                         <div class="ms:w-1/2 md:w-1/3 px-3">
                             <label class="block uppercase tracking-wide text-gray-700 text-xs  mb-2" for="gen_plur">
                                 Plural
                             </label>
-                            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" id="gen_plur" type="text" placeholder="Plural" />
+                            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" id="gen_plur" name="gen_plur" type="text" placeholder="Plural" />
                         </div>
                     </div>
 
@@ -97,11 +111,11 @@ export default function AddWord() {
                                 Dativ
                             </label>
                             <div class="relative">
-                                <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                                <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state" name="dativ_artikle">
                                     <option selected disabled>Artikel</option>
-                                    <option>dem</option>
-                                    <option>der</option>
-                                    <option>den</option>
+                                    <option value="dem">dem</option>
+                                    <option value="der">der</option>
+                                    <option value="den">den</option>
                                 </select>
                                 <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                                     <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
@@ -112,13 +126,13 @@ export default function AddWord() {
                             <label class="block uppercase tracking-wide text-gray-700 text-xs  mb-2" for="dativ_sing">
                                 Singular
                             </label>
-                            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" id="dativ_sing" type="text" placeholder="Singular" />
+                            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" id="dativ_sing" name="dativ_sing" type="text" placeholder="Singular" />
                         </div>
                         <div class="ms:w-1/2 md:w-1/3 px-3">
                             <label class="block uppercase tracking-wide text-gray-700 text-xs  mb-2" for="dativ_plur">
                                 Plural
                             </label>
-                            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" id="dativ_plur" type="text" placeholder="Plural" />
+                            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" id="dativ_plur" name="dativ_plur" type="text" placeholder="Plural" />
                         </div>
                     </div>
 
@@ -131,11 +145,11 @@ export default function AddWord() {
                                 Akkusativ
                             </label>
                             <div class="relative">
-                                <select class="block appearance-none w-full  bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                                <select class="block appearance-none w-full  bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state" name="akkusativ_artikle">
                                     <option selected disabled>Artikel</option>
-                                    <option>den</option>
-                                    <option>das</option>
-                                    <option>die</option>
+                                    <option value="den">den</option>
+                                    <option value="das">das</option>
+                                    <option value="die">die</option>
                                 </select>
                                 <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                                     <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
@@ -146,13 +160,13 @@ export default function AddWord() {
                             <label class="block uppercase tracking-wide text-gray-700 text-xs  mb-2" for="akkusativ_sing">
                                 Singular
                             </label>
-                            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" id="akkusativ_sing" type="text" placeholder="Singular" />
+                            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" id="akkusativ_sing" name="akkusativ_sing" type="text" placeholder="Singular" />
                         </div>
                         <div class="ms:w-1/2 md:w-1/3 px-3">
                             <label class="block uppercase tracking-wide text-gray-700 text-xs  mb-2" for="akkusativ_plur">
                                 Plural
                             </label>
-                            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" id="akkusativ_plur" type="text" placeholder="Plural" />
+                            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" id="akkusativ_plur" name="akkusativ_plur" type="text" placeholder="Plural" />
                         </div>
                     </div>
                     <input type="submit" value="Save" className='w-fit pointer-events-auto rounded-md bg-indigo-600 px-3 py-2 text-[0.8125rem] font-semibold leading-5 text-white hover:bg-indigo-500 cursor-pointer select-none m-3 block' />
