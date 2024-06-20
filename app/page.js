2ef6e2a91e1dec69dd5a46e80136f48e6342b49b
sidhtc510 <sidhtc510@gmail.com>
2024-06-20 17:17:36 +0200

    .

--- app/page.js
+++ app/page.js
@@ -6,16 +6,19 @@ import SearchBar from './components/SearchBar';
 import Link from 'next/link';
 
 const MainWord = ({ letter, words }) => (
-  <div className="mb-8">
-    <h2 className='text-xl font-bold mb-4'>{letter}</h2>
-    <div className="flex gap-5 flex-wrap">
-      {words.map((wordObj, index) => (
-        <div key={index} className={`w-fit pointer-events-auto rounded-md  px-3 py-2 text-[0.8125rem] font-semibold leading-5  cursor-pointer border border-gray-600 select-none ${wordObj.singular.nominativ.substr(0, 3) === 'der' ? 'bg-indigo-300' : wordObj.singular.nominativ.substr(0, 3) === 'die' ? 'bg-red-300' : 'bg-emerald-300'}`}>
-          <WordModal wordObj={wordObj} />
-        </div>
-      ))}
+  <>
+
+    <div className="mb-8">
+      <h2 className='text-xl font-bold mb-4'>{letter}</h2>
+      <div className="flex gap-5 flex-wrap">
+        {words.map((wordObj, index) => (
+          <div key={index} className={`w-fit pointer-events-auto rounded-md px-3 py-2 text-[0.8125rem] font-semibold leading-5  cursor-pointer border border-gray-600 select-none ${wordObj.singular.nominativ.substr(0, 3) === 'der' ? 'bg-indigo-300' : wordObj.singular.nominativ.substr(0, 3) === 'die' ? 'bg-red-300' : 'bg-emerald-300'}`}>
+            <WordModal wordObj={wordObj} />
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
+  </>
 );
 
 export default async function Home() {
@@ -25,9 +28,13 @@ export default async function Home() {
     <>
       <div className='pb-40'>
         <Link href={'/addword'} className='w-fit pointer-events-auto rounded-md bg-indigo-600 px-3 py-2 text-[0.8125rem] font-semibold leading-5 text-white hover:bg-indigo-500 cursor-pointer select-none m-3 block'>Add Word</Link>
+
+
         {Object.entries(words).map(([letter, wordsArray]) => (
           (letter !== '_id' && wordsArray.length > 0) && (
-            <MainWord key={letter} letter={letter} words={wordsArray} />
+            <>
+              <MainWord key={letter} letter={letter} words={wordsArray} />
+            </>
           )
         ))}
       </div>
