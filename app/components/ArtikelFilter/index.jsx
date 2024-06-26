86cab4b076208bd88bc90677e4c70725026360ba
sidhtc510 <sidhtc510@gmail.com>
2024-06-26 15:48:50 +0200

    .

--- app/components/ArtikelFilter/index.jsx
+++ app/components/ArtikelFilter/index.jsx
@@ -6,7 +6,7 @@ import { filterByArticle } from '@/app/store/slice/wordsSlice'
 export default function ArtikelFilter() {
     const dispatch = useDispatch()
     return (
-        <div className='flex justify-center gap-3 flex-col fixed right-0 top-[40vh]'>
+        <div className='flex justify-center gap-3 flex-col fixed right-0 top-[40vh]  bg-white/80 p-2 pl-2 pr-1 rounded-l-md border border-gray-300'>
             <div className='rounded-lg p-2 text-sm text-white bg-indigo-300 cursor-pointer select-none shadow-lg' onClick={() => dispatch(filterByArticle('der'))}> der</div>
             <div className='rounded-lg p-2 text-sm text-white bg-red-300 cursor-pointer select-none shadow-lg' onClick={() => dispatch(filterByArticle('die'))}> die</div>
             <div className='rounded-lg p-2 text-sm text-white bg-emerald-300 cursor-pointer select-none shadow-lg' onClick={() => dispatch(filterByArticle('das'))}> das</div>
