f9461246ce4b92b59a9d5426dc652754dbe55751
sidhtc510 <sidhtc510@gmail.com>
2024-06-29 01:14:38 +0200

    .

--- app/components/SearchBar/index.jsx
+++ app/components/SearchBar/index.jsx
@@ -1,7 +1,9 @@
 'use client'
 import { search } from '@/app/store/slice/wordsSlice';
+import Link from 'next/link';
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux';
+import { PiInfinityThin } from "react-icons/pi";
 
 export default function SearchBar() {
     const dispatch = useDispatch();
@@ -11,8 +13,16 @@ export default function SearchBar() {
     };
 
     return (
-        <div className=' fixed w-full bg-indigo-300 z-10 bottom-0 border border-indigo-600 p-5'>
-            <input type="text" className='w-full rounded-lg p-4 text-xxl text-indigo-900 font-bold' placeholder='Nachschlagen das Wort' onChange={handleSearch} />
-        </div>
+        <>
+            <div className=' fixed w-full bg-indigo-300 z-10 bottom-8 border border-indigo-600 p-5'>
+                <input type="text" className='w-full rounded-lg p-4 text-xxl text-indigo-900 font-bold' placeholder='Nachschlagen das Wort' onChange={handleSearch} />
+            </div>
+            <div className='fixed bottom-0 w-full text-sm font-thin bg-gray-300 text-gray-500 flex align-middle justify-center p-2'>
+                Developed by
+                <Link className='text-gray-500 underline cursor-pointer font-normal pl-1 flex align-bottom relative' href='https://www.linkedin.com/in/sidhtc510/'>
+                    <PiInfinityThin className='mt-[3px] ' /> a$id
+                </Link>
+            </div>
+        </>
     )
 }
