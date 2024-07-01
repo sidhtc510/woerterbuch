bdcf55140a128be7a9f2251ba764cd8b826b6a4d
sidhtc510 <sidhtc510@gmail.com>
2024-07-01 21:12:10 +0200

    .

--- app/components/MobileMenu/index.jsx
+++ app/components/MobileMenu/index.jsx
@@ -1,7 +1,8 @@
 "use client"
 import Link from 'next/link';
 import React, { useState } from 'react'
-import { PiMagicWand } from "react-icons/pi";
+// import { PiMagicWand } from "react-icons/pi";
+import { BsStars } from "react-icons/bs";
 
 export default function MobileMenu() {
     const [isOpen, setIsOpen] = useState(false);
@@ -20,7 +21,7 @@ export default function MobileMenu() {
 
             <div className={`fixed w-full left-0 top-24 h-[calc(100vh-96px)] bg-slate-200 flex flex-col items-center justify-start gap-1 z-20 ${isOpen ? 'visible opacity-100' : 'invisible opacity-0'} ease-in-out duration-500 `}>
                 <Link href={'/addword'} onClick={handleLinkClick} className='w-fit pointer-events-auto rounded-md bg-indigo-600 px-3 py-2 text-[0.8125rem] font-semibold leading-5 text-white hover:bg-indigo-500 cursor-pointer select-none m-3 block'>ein neues Wort hinzufügen</Link>
-                <Link href={'/ai-generator'} onClick={handleLinkClick} className='w-fit flex items-center gap-1 pointer-events-auto rounded-md bg-indigo-600 px-3 py-2 text-[0.8125rem] font-semibold leading-5 text-white hover:bg-indigo-500 cursor-pointer select-none m-3'>AI Generator <PiMagicWand className="text-orange-600 text-lg" /></Link>
+                <Link href={'/ai-generator'} onClick={handleLinkClick} className='w-fit flex items-center gap-1 pointer-events-auto rounded-md bg-indigo-600 px-3 py-2 text-[0.8125rem] font-semibold leading-5 text-white hover:bg-indigo-500 cursor-pointer select-none m-3'>AI Generator <BsStars className="text-orange-600 text-lg" /></Link>
             </div>
         </>
     )
--- app/components/SearchBar/index.jsx
+++ app/components/SearchBar/index.jsx
@@ -4,8 +4,10 @@ import Link from 'next/link';
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { PiInfinityThin, PiMagicWand } from "react-icons/pi";
+import { BsStars } from "react-icons/bs";
 import aiHandler from '@/app/fetches';
 import WordModal from '../WordModal';
+
 
 export default function SearchBar() {
     const dispatch = useDispatch();
@@ -51,7 +53,7 @@ export default function SearchBar() {
                 <input type="text" className='w-full rounded-md p-2 text-xxl text-indigo-800 my-3 ml-3 outline-none' placeholder='Nachschlagen das Wort' value={word} onChange={handleSearch} />
 
                 <button onClick={generateStory} disabled={loading} className="w-fit pointer-events-auto rounded-md bg-indigo-600 px-3 py-2 text-[0.8125rem] font-semibold leading-5 text-white hover:bg-indigo-500 cursor-pointer select-none m-3 block">
-                    {loading ? <p className="text-gray-400">Generating...</p> : (<p className="flex items-center gap-1">Generate <PiMagicWand className="text-orange-600 text-2xl" /></p>)}
+                    {loading ? <p className="text-gray-400">Generating...</p> : (<p className="flex items-center gap-1">Generate <BsStars className="text-orange-600 text-2xl" /></p>)}
                 </button>
 
                 {story && (
@@ -61,10 +63,9 @@ export default function SearchBar() {
                     </>
                 )}
             </div>
-            <div className='fixed bottom-0 w-full text-sm font-thin bg-gray-300 text-gray-500 flex align-middle justify-center p-2'>
+            <div className='fixed bottom-0 w-full text-sm font-thin bg-slate-200 text-gray-500 flex align-middle justify-center p-2'>
                 Developed by
-                <Link className='text-gray-500 underline cursor-pointer pl-1 flex align-bottom relative' href='https://www.linkedin.com/in/sidhtc510/'> a$id
-                </Link>
+                <Link className='text-gray-500 underline cursor-pointer pl-1 flex align-bottom relative' href='https://www.linkedin.com/in/sidhtc510/'> a$id</Link>
             </div>
         </>
     )
