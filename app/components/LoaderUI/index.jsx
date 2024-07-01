6e55e86036dcd0f584c5c7267e9a757e2a72f4b2
sidhtc510 <sidhtc510@gmail.com>
2024-07-01 16:22:26 +0200

    .

--- app/components/LoaderUI/index.jsx
+++ app/components/LoaderUI/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function LoaderUI() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-screen z-50">
         <div role="status">
           <svg className="inline w-8 h-8 text-indigo-600 animate-spin" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g transform="translate(50 50)">
