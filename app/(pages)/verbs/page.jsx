1422bd64afbe5d20dd47b715ebdc154c86df8cdb
sidhtc510 <sidhtc510@gmail.com>
2024-07-09 01:39:41 +0200

    .

--- app/(pages)/verbs/page.jsx
+++ app/(pages)/verbs/page.jsx
@@ -23,8 +23,8 @@ export default async function VerbsPage() {
     alphabet.sort();
 
     return (
-        // <div className='pb-40 bg-slate-200 min-h-[calc(100vh-96px)] '>
-        //     {/* <div className="mb-8 px-4 md:px-12"> */}
+        <div className='pb-40 bg-slate-200 min-h-[calc(100vh-96px)] '>
+            <div className="mb-8 px-4 md:px-12">
 
                 <div className='alphabet_navigation bg-indigo-300 text-2xl flex gap-4 flex-wrap w-full top-0 p-4'>
                     {alphabet.map((letter, index) => (
@@ -35,10 +35,10 @@ export default async function VerbsPage() {
                     ))}
                 </div>
 
-        //         <div className="flex gap-5 flex-wrap mb-8 px-4 my-2 md:px-12">
-        //             {verbs.map(({ _id, verb, translation }, index) => <p className='w-fit pointer-events-auto rounded-md px-3 py-2 text-[0.8125rem] font-semibold leading-5  cursor-pointer  select-none bg-indigo-300' key={index}>{verb} ({translation})</p>)}
-        //         </div>
-        //     {/* </div> */}
-        // </div>
+                <div className="flex gap-5 flex-wrap mb-8 px-4 my-2 md:px-12">
+                    {verbs.map(({ _id, verb, translation }, index) => <p className='w-fit pointer-events-auto rounded-md px-3 py-2 text-[0.8125rem] font-semibold leading-5  cursor-pointer  select-none bg-indigo-300' key={index}>{verb} ({translation})</p>)}
+                </div>
+            </div>
+        </div>
     )
 }
