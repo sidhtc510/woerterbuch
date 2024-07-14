4698dc5d393018f2d775af6ca57a03d59230133f
sidhtc510 <sidhtc510@gmail.com>
2024-07-14 17:19:43 +0200

    .

--- app/fetchVerbs.js
+++ app/fetchVerbs.js
@@ -38,35 +38,71 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 export default async function aiHandlerVerb(req) {
     const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY);
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", generationConfig: { responseMimeType: "application/json", } });
 
-    const generationConfig = {
-        temperature: 0.4,
-        topP: 0.95,
-        topK: 64,
-        maxOutputTokens: 8192,
-        // responseMimeType: "text/plain",
-        responseMimeType: "application/json",
-    };
+    // const generationConfig = {
+    //     temperature: 0.4,
+    //     topP: 0.95,
+    //     topK: 64,
+    //     maxOutputTokens: 8192,
+    //     // responseMimeType: "text/plain",
+    //     responseMimeType: "application/json",
+    // };
 
-    const chatSession = await model.startChat({
-        generationConfig,
-        history: [
-            {
-                role: "user",
-                parts: [
-                    // {
-                    //     text: "Я даю тебе глагол. переведи его на немецкий язык. напиши json объект. Строго соблюдай структуру, ключи объекта должны быть в двойных ковычках \" . examples приведи от одного до пяти примеров. notes - объяснение должно быть на русском. в ключ verb запиши слово на немецком, а в ключ translation его перевод. ниже пример объекта {\"verb\": \"machen\",\"translation\": \"делать\",\"conjugation\": {\"present\": {\"ich\": \"mache\",\"du\": \"machst\",\"er_sie_es\": \"macht\",\"wir\": \"machen\",\"ihr\": \"macht\",\"sie_Sie\": \"machen\"},\"past\": {\"ich\": \"machte\",\"du\": \"machtest\",\"er_sie_es\": \"machte\",\"wir\": \"machten\",\"ihr\": \"machtet\",\"sie_Sie\": \"machten\"},\"future\": {\"ich\": \"werde machen\",\"du\": \"wirst machen\",\"er_sie_es\": \"wird machen\",\"wir\": \"werden machen\",\"ihr\": \"werdet machen\",\"sie_Sie\": \"werden machen\"},},\"examples\": [\"Ich mache meine Hausaufgaben.\",\"Er hat das Auto gemacht.\"],\"notes\": \"Глагол 'machen' используется в широком спектре значений, включая 'создавать', 'делать' и 'выполнять'.\"} "
-                    // },
-                    {
-                        text: "Я даю тебе глагол. напиши json объект немецкого глагола. Строго соблюдай структуру. examples приведи от одного до пяти примеров. notes - объяснение должно быть на русском. в ключ verb запиши слово на немецком, а в ключ translation его перевод на русский язык. ниже пример объекта {\"verb\": \"machen\",\"translation\": \"делать\",\"conjugation\": {\"present\": {\"ich\": \"mache\",\"du\": \"machst\",\"er_sie_es\": \"macht\",\"wir\": \"machen\",\"ihr\": \"macht\",\"sie_Sie\": \"machen\"},\"past\": {\"ich\": \"machte\",\"du\": \"machtest\",\"er_sie_es\": \"machte\",\"wir\": \"machten\",\"ihr\": \"machtet\",\"sie_Sie\": \"machten\"},\"future\": {\"ich\": \"werde machen\",\"du\": \"wirst machen\",\"er_sie_es\": \"wird machen\",\"wir\": \"werden machen\",\"ihr\": \"werdet machen\",\"sie_Sie\": \"werden machen\"},},\"examples\": [\"Ich mache meine Hausaufgaben.\",\"Er hat das Auto gemacht.\"],\"notes\": \"Глагол 'machen' используется в широком спектре значений, включая 'создавать', 'делать' и 'выполнять'.\"} "
-                    },
-                ],
-            },
-        ],
-    });
+    // const chatSession = await model.startChat({
+    //     generationConfig,
+    //     history: [
+    //         {
+    //             role: "user",
+    //             parts: [
+    //                 // {
+    //                 //     text: "Я даю тебе глагол. переведи его на немецкий язык. напиши json объект. Строго соблюдай структуру, ключи объекта должны быть в двойных ковычках \" . examples приведи от одного до пяти примеров. notes - объяснение должно быть на русском. в ключ verb запиши слово на немецком, а в ключ translation его перевод. ниже пример объекта {\"verb\": \"machen\",\"translation\": \"делать\",\"conjugation\": {\"present\": {\"ich\": \"mache\",\"du\": \"machst\",\"er_sie_es\": \"macht\",\"wir\": \"machen\",\"ihr\": \"macht\",\"sie_Sie\": \"machen\"},\"past\": {\"ich\": \"machte\",\"du\": \"machtest\",\"er_sie_es\": \"machte\",\"wir\": \"machten\",\"ihr\": \"machtet\",\"sie_Sie\": \"machten\"},\"future\": {\"ich\": \"werde machen\",\"du\": \"wirst machen\",\"er_sie_es\": \"wird machen\",\"wir\": \"werden machen\",\"ihr\": \"werdet machen\",\"sie_Sie\": \"werden machen\"},},\"examples\": [\"Ich mache meine Hausaufgaben.\",\"Er hat das Auto gemacht.\"],\"notes\": \"Глагол 'machen' используется в широком спектре значений, включая 'создавать', 'делать' и 'выполнять'.\"} "
+    //                 // },
+    //                 {
+    //                     text: "Я даю тебе глагол. напиши json объект немецкого глагола. Строго соблюдай структуру. examples приведи от одного до пяти примеров. notes - объяснение должно быть на русском. в ключ verb запиши слово на немецком, а в ключ translation его перевод на русский язык. ниже пример объекта {\"verb\": \"machen\",\"translation\": \"делать\",\"conjugation\": {\"present\": {\"ich\": \"mache\",\"du\": \"machst\",\"er_sie_es\": \"macht\",\"wir\": \"machen\",\"ihr\": \"macht\",\"sie_Sie\": \"machen\"},\"past\": {\"ich\": \"machte\",\"du\": \"machtest\",\"er_sie_es\": \"machte\",\"wir\": \"machten\",\"ihr\": \"machtet\",\"sie_Sie\": \"machten\"},\"future\": {\"ich\": \"werde machen\",\"du\": \"wirst machen\",\"er_sie_es\": \"wird machen\",\"wir\": \"werden machen\",\"ihr\": \"werdet machen\",\"sie_Sie\": \"werden machen\"},},\"examples\": [\"Ich mache meine Hausaufgaben.\",\"Er hat das Auto gemacht.\"],\"notes\": \"Глагол 'machen' используется в широком спектре значений, включая 'создавать', 'делать' и 'выполнять'.\"} "
+    //                 },
+    //             ],
+    //         },
+    //     ],
+    // });
 
-    const result = await chatSession.sendMessage(req);
+    const prompt = `
+    Я даю тебе глагол ${req}. напиши json объект немецкого глагола. Строго соблюдай структуру. examples приведи от одного до пяти примеров. notes - объяснение должно быть на русском. в ключ verb запиши слово на немецком, а в ключ translation его перевод на русский язык. ниже пример объекта {"verb": "machen",
+    "translation": "делать",
+    "conjugation": {
+        "present": {
+            "ich": "mache",
+            "du": "machst",
+            "er_sie_es": "macht",
+            "wir": "machen",
+            "ihr": "macht",
+            "sie_Sie": "machen"
+        },
+        "past": {
+            "ich": "machte",
+            "du": "machtest",
+            "er_sie_es": "machte",
+            "wir": "machten",
+            "ihr": "machtet",
+            "sie_Sie": "machten"
+        },
+        "future": {
+            "ich": "werde machen",
+            "du": "wirst machen",
+            "er_sie_es": "wird machen",
+            "wir": "werden machen",
+            "ihr": "werdet machen",
+            "sie_Sie": "werden machen"
+        },
+    },
+    "examples": ["Ich mache meine Hausaufgaben.","Er hat das Auto gemacht."],
+    "notes": "Глагол 'machen' используется в широком спектре значений, включая 'создавать', 'делать' и 'выполнять'."
+    } 
+    `
+
+    // const result = await chatSession.sendMessage(req);
+    const result = await model.generateContent(prompt);
+
 
     // return JSON.parse(result.response.text());
     return result.response.text();
--- app/fetches.js
+++ app/fetches.js
@@ -21,35 +21,61 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 export default async function aiHandler(req) {
     const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY);
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const model = genAI.getGenerativeModel(
+        {
+            model: "gemini-1.5-flash",
+            generationConfig: { responseMimeType: "application/json" }
+        });
 
-    const generationConfig = {
-        temperature: 0.4,
-        topP: 0.95,
-        topK: 64,
-        maxOutputTokens: 8192,
-        // responseMimeType: "text/plain",
-        responseMimeType: "application/json",
-    };
-
-    const chatSession = await model.startChat({
-        generationConfig,
-        history: [
+    const prompt = `
+       вот слово ${req}. переведи его на немецкий и поставь ему правильный артикль в разных падежах в единственном и множественном числе. оформить в объект. ничего не убирай и ничего лишнего не дописывай.  Если у слова нет множественного числа, проставь \"-\", ты должен обрабатывать только существительные, иначе возвращай вместо объекта false. строго следуй структуре объекта. вот пример объекта.
             {
-                role: "user",
-                parts: [
-                    {
-                        text: "я даю слово на немецком языке.  тебе нужно поставить ему правильный артикль в разных падежах в единственном и множественном числе.  и оформить его в объект. вот пример объекта. строго следуй его структуре. ничего не убирай и ничего лишнего не дописывай.  Если у слова нет множественного числа, проставь \"-\", ты должен обрабатывать только существительные, иначе возвращай вместо объекта false \n{\"word\":\"Ofen\",\"wordRu\":\"печь\",\"singular\":{\"nominativ\":\"der Ofen\",\"genitiv\":\"des Ofens\",\"dativ\":\"dem Ofen\",\"akkusativ\":\"den Ofen\"},\"plural\":{\"nominativ\":\"die Öfen\",\"genitiv\": \"der Öfen\",\"dativ\":\"den Öfen\",\"akkusativ\":\"die Öfen\"}},"
-                    },
-                ],
+            "word": "Apfel",
+            "wordRu": "яблоко",
+            "singular": {
+                "nominativ": "der Apfel",
+                "genitiv": "des Apfels",
+                "dativ": "dem Apfel",
+                "akkusativ": "den Apfel"
             },
-        ],
-    });
+            "plural": {
+                "nominativ": "die Äpfel",
+                "genitiv": "der Äpfel",
+                "dativ": "den Äpfeln",
+                "akkusativ": "die Äpfel"
+            }
+        },
+        `
+    const result = await model.generateContent(prompt);
 
-    const result = await chatSession.sendMessage(req);
-
-    // return JSON.parse(result.response.text());
     return result.response.text();
+    // const generationConfig = {
+    //     temperature: 0.4,
+    //     topP: 0.95,
+    //     topK: 64,
+    //     maxOutputTokens: 8192,
+    //     // responseMimeType: "text/plain",
+    //     responseMimeType: "application/json",
+    // };
+
+    // const chatSession = await model.startChat({
+    //     generationConfig,
+    //     history: [
+    //         {
+    //             role: "user",
+    //             parts: [
+    //                 {
+    //                     text: "я даю слово на немецком языке.  тебе нужно поставить ему правильный артикль в разных падежах в единственном и множественном числе.  и оформить его в объект. вот пример объекта. строго следуй его структуре. ничего не убирай и ничего лишнего не дописывай.  Если у слова нет множественного числа, проставь \"-\", ты должен обрабатывать только существительные, иначе возвращай вместо объекта false \n{\"word\":\"Ofen\",\"wordRu\":\"печь\",\"singular\":{\"nominativ\":\"der Ofen\",\"genitiv\":\"des Ofens\",\"dativ\":\"dem Ofen\",\"akkusativ\":\"den Ofen\"},\"plural\":{\"nominativ\":\"die Öfen\",\"genitiv\": \"der Öfen\",\"dativ\":\"den Öfen\",\"akkusativ\":\"die Öfen\"}},"
+    //                 },
+    //             ],
+    //         },
+    //     ],
+    // });
+
+    // const result = await chatSession.sendMessage(req);
+
+    // // return JSON.parse(result.response.text());
+    // return result.response.text();
 }
 
 
