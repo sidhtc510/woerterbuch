8139e70828135c2773f73ca7d178c354683471dc
sidhtc510 <sidhtc510@gmail.com>
2024-07-22 20:37:00 +0200

    .

--- app/(pages)/derdasdie_game/page.jsx
+++ app/(pages)/derdasdie_game/page.jsx
@@ -1,14 +1,16 @@
 'use client'
 import LoaderUI from '@/app/components/LoaderUI'
-import { getRandomWord } from '@/app/helpers/functions'
+import translateViaAi, { getRandomWord } from '@/app/helpers/functions'
 import { fetchWordsNominative } from '@/app/store/slice/derdasdie'
 import { addCorrect, addWord, addWrong } from '@/app/store/slice/derdasdie_score'
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { SlDislike, SlLike } from "react-icons/sl";
+import { MdClose } from "react-icons/md";
 
 export default function DerDasDieGame() {
     const [randomWord, setRandomWord] = useState({ word: '', wordWithArtikle: '' });
+    const [translatedWord, setTranslatedWord] = useState({ isOpen: false, wordRuAi: '' });
 
     const dispatch = useDispatch();
 
@@ -33,6 +35,13 @@ export default function DerDasDieGame() {
         dispatch(addWord({ word: randomWord.wordWithArtikle, isCorrect: isCorrect }));
     }
 
+    const translateHandler = async (word) => {
+        const req_word = await translateViaAi(word);
+        setTranslatedWord({ isOpen: true, wordRuAi: req_word });
+
+    }
+
+
     return (
         <div className='pb-40 bg-slate-200 min-h-[calc(100vh-96px)] '>
             {status !== 'ready' ? <LoaderUI /> :
@@ -43,19 +52,25 @@ export default function DerDasDieGame() {
                                 <div className='flex justify-center gap-3 text-lg font-bold items-center'>
                                     <SlLike className='text-green-600' /> {score.list.score.correct}  <SlDislike className='text-red-600' /> {score.list.score.wrong}
                                 </div>
-                                <div className='mx-auto my-4 text-4xl p-10 text-white bg-sky-400 rounded-lg flex justify-center align-middle w-fit shadow-lg'>
+                                <div className='mx-auto my-4 text-4xl p-10 text-white bg-sky-400 rounded-lg flex justify-center align-middle w-fit shadow-lg relative cursor-pointer' onClick={() => translateHandler(randomWord.word)}>
                                     {randomWord.word}
+                                    {translatedWord.isOpen &&
+                                        <div className='absolute w-[150%] h-[150%] top-0 p-2 border border-blue-600 rounded bg-slate-200 text-blue-600 text-2xl flex items-center justify-center' onClick={e => e.stopPropagation()}>
+                                            <MdClose className='absolute top-0 right-0 p-1 text-2xl cursor-pointer' onClick={(e) => { e.stopPropagation(); setTranslatedWord({ isOpen: false, wordRuAi: '' }) }} />
+                                            {translatedWord.wordRuAi}
+                                        </div>
+                                    }
                                 </div>
                                 <div className='flex justify-center gap-2'>
                                     {['der', 'die', 'das'].map(artikel => (
-                                        <p key={artikel} className={`cursor-pointer px-4 py-6 border rounded-lg ${artikel === 'der' ? 'bg-indigo-300' : artikel === 'die' ? 'bg-red-300' : 'bg-emerald-300'}`} onClick={() => handler_derdasdie_game(artikel)}>{artikel}</p>
+                                        <p key={artikel} className={`cursor-pointer px-4 py-6 rounded-lg ${artikel === 'der' ? 'bg-indigo-300' : artikel === 'die' ? 'bg-red-300' : 'bg-emerald-300'}`} onClick={() => handler_derdasdie_game(artikel)}>{artikel}</p>
                                     ))}
                                 </div>
-                                {score.list.wordsWithArtikle.length > 0 && (
-                                    <div className='flex flex-col gap-2 m-4 px-10 py-2 border-gray-500 rounded-2xl w-fit mx-auto'>
-                                        {score.list.wordsWithArtikle.map((el, index) => <p key={index} className={`border-b border-l rounded-sm py-1 px-2 ${el.isCorrect ? 'border-emerald-300 bg-emerald-100' : 'border-red-300 bg-red-100'}`}> {el.word}</p>)}
+                                {score.list.wordsWithArtikle.length > 0 &&
+                                    <div className='flex flex-col gap-2 m-4 px-10 py-2 w-fit mx-auto'>
+                                        {score.list.wordsWithArtikle.map((el, index) => <p key={index} className={`border-b border-l rounded-sm py-1 px-2 ${el.isCorrect ? 'border-emerald-300 bg-emerald-100' : 'border-red-300 bg-red-100'}`} onClick={() => translateHandler(el.word)}> {el.word}</p>)}
                                     </div>
-                                )}
+                                }
                             </>
                         )}
                     </div>
--- app/helpers/functions.js
+++ app/helpers/functions.js
@@ -39,3 +39,21 @@ export function getRandomWord(arr) {
     const randomIndex = Math.floor(Math.random() * arr.length);
     return { word: arr[randomIndex].slice(4), wordWithArtikle: arr[randomIndex] };
 }
+
+
+
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+export default async function translateViaAi(req) {
+    const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY);
+    const model = genAI.getGenerativeModel(
+        {
+            model: "gemini-1.5-flash",
+            generationConfig: { responseMimeType: "application/json" }
+        });
+
+    const prompt = `Переведи немецкое "${req}" на русский язык. Верни string.`
+    const result = await model.generateContent(prompt);
+
+    return result.response.text();
+}
