b13132c0aecc5b241bc12bfaac4f2e6a6f5ba33d
sidhtc510 <sidhtc510@gmail.com>
2024-07-22 19:27:19 +0200

    .

--- app/components/DevelopedbySid/index.jsx
+++ app/components/DevelopedbySid/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function DevelopedbySid() {
     return (
-        <div className='fixed bottom-0 w-full text-sm font-thin bg-slate-200 text-gray-500 flex align-middle justify-center p-2'>
+        <div className='fixed gap-1 bottom-0 w-full text-sm font-thin bg-slate-200 text-gray-500 flex align-middle justify-center p-2'>
             Developed by
             <Link className='text-gray-500 underline cursor-pointer pl-1 flex align-bottom relative' href='https://www.linkedin.com/in/sidhtc510/'> a$id</Link>
         </div>
